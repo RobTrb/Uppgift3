27d092173de3fa274e83c823127ad73d405c6214
RobTrb <robert.trb@gmail.com>
2024-09-11 10:33:30 +0200

    Added function and event listener for "dynamic"

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ function change() {
 }
 
 function dynamicChange() {
-    if (document.getElementById("world").innerHTML="Hello World") {
-        document.getElementById("world").innerHTML="Goodbye World"
-    }
-        else if (document.getElementById("world").innerHTML="Goodbye World"){
+    if (document.getElementById("world").innerHTML="Hello World") 
+            {
+            document.getElementById("world").innerHTML="Goodbye World"
+            }
+        else if (document.getElementById("world").innerHTML="Goodbye World")
+            {
             document.getElementById("World").innerHTML="Hello World"
-        }
+            }
         
 }
+dynamicButton.addEventListener('click', dynamicChange);
