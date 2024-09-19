e4c68b2995ae94e9a11f9263dd11b7bcf958746e
RobTrb <robert.trb@gmail.com>
2024-09-19 15:20:51 +0200

    Final changes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const dynamicButton = document.getElementById('dynBtn')
+const changeButton = document.querySelector("#changeBtn");
+const dynamicButton = document.querySelector("#dynBtn");
+const h1 = document.querySelector("#world");
 
 function change() {
-    document.getElementById("world").innerHTML="Goodbye World"
+    document.querySelector("#world").innerHTML="Goodbye World"
 }
 
 function dynamicChange() {
-    if (document.getElementById("world").innerHTML="Hello World") 
-            {
-            document.getElementById("world").innerHTML="Goodbye World"
-            }
-        else if (document.getElementById("world").innerHTML="Goodbye World")
-            {
-            document.getElementById("World").innerHTML="Hello World"
-            }
+    if (h1.textContent === "Hello World") {
+        h1.textContent = "Goodbye World";
+    } else {
+        h1.textContent = "Hello World"
+    }
         
 }
+changeButton.addEventListener('click', change);
 dynamicButton.addEventListener('click', dynamicChange);
